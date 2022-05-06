985827da0933929ed7c7fbadae39d1258c1dc40d
Ru-Ri-z <69484119+Ru-Ri-z@users.noreply.github.com>
2022-05-06 13:26:52 -0300

    up2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import './App.css'
 import Profile from './webcontainer/home/Profile'
+import React from 'react'
+
+
 
 export default function App() {
   return (
--- src/webcontainer/home/Profile.js
+++ src/webcontainer/home/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Typical from 'react-typical'
 
 export default function Profile() {
   return (
@@ -6,8 +7,54 @@ export default function Profile() {
         <div className='profile-parent'>
             <div className='profile-details'>
                 <div className='colz'>
-                    
+                    <a href='#'>
+                        <i className='fa fa-facebook-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-google-plus-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-youtube-square'></i>
+                    </a>
+                    <a href='#'>
+                        <i className='fa fa-twitter'></i>
+                    </a>
                 </div>
+
+                <div className='profile-details-name'>
+                    <span className='primary-text'>
+                        {" "}
+                        Hola, yo soy <span className='highlighted-text'>tu vieja</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                            <Typical
+                            loop={Infinity}
+                            steps={[
+                                "Surf the Latam Digital Policy Scene",
+                                1000,
+                            ]}
+                            />
+                        </h1>
+                        <span className='prrofile-role-tagline'>
+                            kldsfkjsdhfjkdshfjdshfjdshfsdjhfdsjhfdshjfsdjkf
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options'>
+                    <button className='btn primary-btn'>
+                        {""}
+                        Hire me {" "}
+                    </button>
+                    <a href="#"></a>
+                </div>
+
             </div>
         </div>
     </div>
